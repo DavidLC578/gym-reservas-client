14622b0786f435cc8b059e5260d67a04f5a7d01f
DavidLC578 <davidlatt2002@gmail.com>
2025-03-19 12:50:15 -0400

    feat: navbar (incompleted)

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
+  <NavBarComponent />
   <router-view />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import './assets/tailwind.css';
+import NavBarComponent from './components/NavBarComponent.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
+    NavBarComponent
   }
 });
 </script>
 
 <style>
+body {
+  background: rgb(229, 229, 229);
+}
 </style>
---
+++ src/components/NavBarComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="flex justify-between items-center w-full bg-white p-4">
+    <span class="text-xl font-bold">GYM</span>
+    <ul class="flex justify-center space-x-7 font-semibold">
+      <li :class="{ 'relative': true }">
+        <router-link to="/" class="text-lg">Welcome</router-link>
+        <div :class="{ 'absolute top-7 left-0 w-full h-1 bg-purple-500 transform transition-all duration-300 scale-x-100': isActive('/') }"></div>
+      </li>
+      <li :class="{ 'relative': true }">
+        <router-link to="/classes" class="text-lg">Classes</router-link>
+        <div :class="{ 'absolute top-7 left-0 w-full h-1 bg-purple-500 transform transition-all duration-300 scale-x-100': isActive('/classes') }"></div>
+      </li>
+      <li :class="{ 'relative': true }">
+        <router-link to="/join" class="text-lg">Join the Gym</router-link>
+        <div :class="{ 'absolute top-7 left-0 w-full h-1 bg-purple-500 transform transition-all duration-300 scale-x-100': isActive('/join') }"></div>
+      </li>
+    </ul>
+    <ul class="flex justify-center space-x-7 font-semibold">
+      <li class="relative">
+        <router-link to="/login" class="text-lg">Login</router-link>
+      </li>
+      <li class="relative">
+        <router-link to="/register" class="text-lg">Register</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  methods: {
+    isActive(route) {
+      return this.$route.path === route;
+    }
+  }
+};
+</script>
