3dc2c157fb30c5d97393fd72ee7dc327766b0f7e
DavidLC578 <davidlatt2002@gmail.com>
2025-03-19 13:22:03 -0400

    fix: navbar

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -1,37 +1,60 @@
 <template>
-  <nav class="flex justify-between items-center w-full bg-white p-4">
-    <span class="text-xl font-bold">GYM</span>
-    <ul class="flex justify-center space-x-7 font-semibold">
-      <li :class="{ 'relative': true }">
-        <router-link to="/" class="text-lg">Welcome</router-link>
-        <div :class="{ 'absolute top-7 left-0 w-full h-1 bg-purple-500 transform transition-all duration-300 scale-x-100': isActive('/') }"></div>
-      </li>
-      <li :class="{ 'relative': true }">
-        <router-link to="/classes" class="text-lg">Classes</router-link>
-        <div :class="{ 'absolute top-7 left-0 w-full h-1 bg-purple-500 transform transition-all duration-300 scale-x-100': isActive('/classes') }"></div>
-      </li>
-      <li :class="{ 'relative': true }">
-        <router-link to="/join" class="text-lg">Join the Gym</router-link>
-        <div :class="{ 'absolute top-7 left-0 w-full h-1 bg-purple-500 transform transition-all duration-300 scale-x-100': isActive('/join') }"></div>
-      </li>
-    </ul>
-    <ul class="flex justify-center space-x-7 font-semibold">
-      <li class="relative">
-        <router-link to="/login" class="text-lg">Login</router-link>
-      </li>
-      <li class="relative">
-        <router-link to="/register" class="text-lg">Register</router-link>
-      </li>
-    </ul>
-  </nav>
+    <nav class="lg:flex lg:justify-between items-center w-full bg-white p-4">
+        <div class="flex justify-between items-center">
+            <span class="text-xl font-bold">GYM</span>
+            <button @click="toggleMenu" class="lg:hidden p-2">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                </svg>
+            </button>
+        </div>
+        <ul :class="{ 'hidden': !isMenuOpen, 'flex-col': isMenuOpen }"
+            class="flex lg:flex-row justify-center  md:items-center lg:space-x-7 font-semibold py-2 space-y-2">
+            <li :class="{ 'relative': true }">
+                <router-link to="/" class="text-lg">Welcome</router-link>
+                <div
+                    :class="{ 'absolute top-7 left-0 w-full h-1 bg-purple-500 transform transition-all duration-300 scale-x-100': isActive('/') }">
+                </div>
+            </li>
+            <li :class="{ 'relative': true }">
+                <router-link to="/classes" class="text-lg">Classes</router-link>
+                <div
+                    :class="{ 'absolute top-7 left-0 w-full h-1 bg-purple-500 transform transition-all duration-300 scale-x-100': isActive('/classes') }">
+                </div>
+            </li>
+            <li :class="{ 'relative': true }">
+                <router-link to="/join" class="text-lg">Join the Gym</router-link>
+                <div
+                    :class="{ 'absolute top-7 left-0 w-full h-1 bg-purple-500 transform transition-all duration-300 scale-x-100': isActive('/join') }">
+                </div>
+            </li>
+        </ul>
+        <ul class="lg:flex justify-center space-x-7 font-semibold hidden">
+            <li class="relative">
+                <router-link to="/login" class="text-lg">Login</router-link>
+            </li>
+            <li class="relative">
+                <router-link to="/register" class="text-lg">Register</router-link>
+            </li>
+        </ul>
+    </nav>
 </template>
 
 <script>
 export default {
-  methods: {
-    isActive(route) {
-      return this.$route.path === route;
+    data() {
+        return {
+            isMenuOpen: false
+        };
+    },
+    methods: {
+        toggleMenu() {
+            this.isMenuOpen = !this.isMenuOpen;
+        },
+        isActive(route) {
+            return this.$route.path === route;
+        }
     }
-  }
 };
 </script>
